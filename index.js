27ba5385179ac19c1e65cc9a944da3a8051f0a0a
mohdkassar <mohamad.shabeb@shabeb.com>
2020-11-23 11:32:56 +0200

    sds

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ var express = require("express");
 var cors = require("cors");
 var app = express();
 var mongoose = require("mongoose");
+var fileupload = require("express-fileupload");
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(cors());
-
+app.use(fileupload());
 app.listen(8080, () => {
   console.log("listening");
 });
