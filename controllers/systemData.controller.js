be67a5c59936e487f8b478c19debc72f9d7326eb
mohdkassar <mohamad.shabeb@shabeb.com>
2020-12-01 19:52:46 +0200

    vbn

--- controllers/systemData.controller.js
+++ controllers/systemData.controller.js
@@ -149,9 +149,11 @@ exports.getLatestSystemValues = (req, res) => {
             .exec(function (err, imageDocs) {
               docs[0].data[0]["PlantName"] = user.tray1;
               docs[0].data[1]["PlantName"] = user.tray2;
-              docs[0].data[0]["filePath"] = imageDocs[0].data.filePath;
-              docs[0].data[1]["filePath"] = imageDocs[0].data.filePath;
-              console.log(imageDocs[0].data.filePath);
+              if (imageDocs[0].data != null) {
+                docs[0].data[0]["filePath"] = imageDocs[0].data.filePath;
+                docs[0].data[1]["filePath"] = imageDocs[0].data.filePath;
+                console.log(imageDocs[0].data.filePath);
+              }
               res.send(docs);
             });
         });
