026cfcc09cfd3d22006c8d4a1d01cf9c1c68f883
mohdkassar <mohamad.shabeb@shabeb.com>
2020-12-02 08:14:14 +0200

    ttt

--- mqtt/client.js
+++ mqtt/client.js
@@ -23,56 +23,60 @@ client.on("message", function (topic, message) {
   console.log(topic);
   if (message.length > 0) {
     console.log(message.toString);
-    var mqttMessage = JSON.parse(message.toString());
-    console.log(mqttMessage.message.localeCompare("SR"));
-    if (mqttMessage.message.localeCompare("SR") == 0) {
-      console.log("------");
-      const systemData = new SystemData({
-        user_id: 1664,
-        dataType: "Sensor Reading",
-        data: [
-          {
-            pH: mqttMessage.TR1[5],
-            EC: mqttMessage.TR1[3],
-            "Water Level": mqttMessage.TR1[1],
-            Temperature: mqttMessage.GR[1],
-          },
-          {
-            pH: mqttMessage.TR2[5],
-            EC: mqttMessage.TR2[3],
-            "Water Level": mqttMessage.TR2[1],
-            Temperature: mqttMessage.GR[1],
-          },
-        ],
-      });
-      systemData
-        .save()
-        .then((data) => {
-          console.log(data);
-        })
-        .catch((err) => {
-          console.log(err.message);
+    try {
+      var mqttMessage = JSON.parse(message.toString());
+      console.log(mqttMessage.message.localeCompare("SR"));
+      if (mqttMessage.message.localeCompare("SR") == 0) {
+        console.log("------");
+        const systemData = new SystemData({
+          user_id: 1664,
+          dataType: "Sensor Reading",
+          data: [
+            {
+              pH: mqttMessage.TR1[5],
+              EC: mqttMessage.TR1[3],
+              "Water Level": mqttMessage.TR1[1],
+              Temperature: mqttMessage.GR[1],
+            },
+            {
+              pH: mqttMessage.TR2[5],
+              EC: mqttMessage.TR2[3],
+              "Water Level": mqttMessage.TR2[1],
+              Temperature: mqttMessage.GR[1],
+            },
+          ],
         });
-    } else if (mqttMessage.message.localeCompare("SI") == 0) {
-      console.log("------");
-      const systemData = new SystemData({
-        user_id: 1664,
-        dataType: "System Information",
-        data: {
-          status: mqttMessage.SS,
-          solution1: mqttMessage.BS[0],
-          solution2: mqttMessage.BS[1],
-          solution3: mqttMessage.BS[2],
-        },
-      });
-      systemData
-        .save()
-        .then((data) => {
-          console.log(data);
-        })
-        .catch((err) => {
-          console.log(err.message);
+        systemData
+          .save()
+          .then((data) => {
+            console.log(data);
+          })
+          .catch((err) => {
+            console.log(err.message);
+          });
+      } else if (mqttMessage.message.localeCompare("SI") == 0) {
+        console.log("------");
+        const systemData = new SystemData({
+          user_id: 1664,
+          dataType: "System Information",
+          data: {
+            status: mqttMessage.SS,
+            solution1: mqttMessage.BS[0],
+            solution2: mqttMessage.BS[1],
+            solution3: mqttMessage.BS[2],
+          },
         });
+        systemData
+          .save()
+          .then((data) => {
+            console.log(data);
+          })
+          .catch((err) => {
+            console.log(err.message);
+          });
+      }
+    } catch (e) {
+      console.log(e);
     }
   }
 });
